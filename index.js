02073362ae705f71c136288f0621475e1dd78809
Justin Jones <jus.jones@yahoo.com>
2022-03-12 11:18:23 -0500

    commit tree 7f078ba59b40a29764a5118f0faacfcb0256b0d9

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let multiply 
+let num1 = 31
+let num2 = 2
+ multiply = num1 * num2
+
+ let random
+
+random = Math.floor(Math.random() + 1);
+
+console.log(random);
+
+const num3 = 31;
+const num4 = 9;
+const mod = num3 % num4;
+
+const max = Math.max(4,5,6,20);
 
 // Write your code here
